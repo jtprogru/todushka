6f67e08d910a4f3c2b696f8f7aaf33bb4f5faf69
Michael Savin <jtprogru@gmail.com>
2023-02-09 22:13:58 +0300

    fix: update init.sql

--- init.sql
+++ init.sql
@@ -9,13 +9,12 @@ CREATE TABLE todo_items
     id          serial       not null unique,
     summary     varchar(255) not null,
     description varchar(255),
-    is_done     boolean      not null default false,
-    list_id     varchar(32)
+    is_done     boolean      not null default false
 );
 
-INSERT INTO todo_items (summary, description, is_done, list_id)
+INSERT INTO todo_items (summary, description, is_done)
 VALUES
-('Todo 1 Hello world', 'Todo 1 Hello world from planet earth', false, '1'),
-('Todo 2 Hello world', 'Todo 2 Hello world from planet earth', false, '1'),
-('Todo 3 Hello world', 'Todo 3 Hello world from planet earth', false, '2'),
-('Todo 4 Hello world', 'Todo 4 Hello world from planet earth', false, '2');
+    ('Todo 1 Hello world', 'Todo 1 Hello world from planet earth', false),
+    ('Todo 2 Hello world', 'Todo 2 Hello world from planet earth', false),
+    ('Todo 3 Hello world', 'Todo 3 Hello world from planet earth', false),
+    ('Todo 4 Hello world', 'Todo 4 Hello world from planet earth', false);
